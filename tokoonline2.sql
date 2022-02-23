ec4068a86c873bad89109e1a566b40ac2052b6ed
mdwiastika <marceldwias@gmail.com>
2022-02-23 15:55:18 +0700

    memperbarui sql

--- tokoonline2.sql
+++ tokoonline2.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 21, 2022 at 05:29 AM
+-- Generation Time: Feb 23, 2022 at 09:54 AM
 -- Server version: 10.4.20-MariaDB
 -- PHP Version: 8.0.8
 
@@ -43,13 +43,13 @@ CREATE TABLE `barang` (
 --
 
 INSERT INTO `barang` (`id`, `nama`, `stok`, `harga`, `kategori_id`, `image`, `detail`, `berat`) VALUES
-(30, 'Pocophone X3 NFC', 74, 4000000, '2', '61f7543fcaa7b.png', 'ditawarkan oleh prosesor baru. Prosesor Qualcomm® Snapdragon™ 732G menawarkan kapasitas pemrosesan grafis yang kuat dan kemampuan komputasi AI yang canggih. Teknologi proses 8nm dan chip octa-core yang membantu mencapai kecepatan jam 2,3GHz.', 2500),
-(31, 'Mi Band 5', 74, 400000, '9', '61f78aa6876ef.png', 'Baterai 500 mAh, tahan air. Bisa tahan 10 hari pemakaian normal. Tersedia berbagai warna', 1200),
-(32, 'Canon', 67, 3500000, '7', '61f78c64beb65.png', 'Sensor APS-C CMOS 24,1 megapiksel+ perekaman video 4K 45 titik AF semua tipe silang (jendela bidik) dan Dual Pixel CMOS AF (Live View)', 3000),
-(34, 'Lenovo Legion 5', 48, 19000000, '3', '61fa1ee09b2fa.png', 'Engineered to deliver devastation in and out of the arena, the Legion 5 Pro deploys AMD Ryzen™ processing and NVIDIA® GeForce RTX™ graphics to dish out high-resolution gaming. The world’s first 16&amp;quot; QHD gaming laptop with up to 165Hz refresh sets up a “winning zone” that gives you an extra e', 1500),
-(35, 'Pop It', 119, 20000, '6', '61fccbe0b0ec4.png', 'Ini adalah mainan yang viral pada masanya', 200),
-(36, 'Televisi Panasonic', 55, 3500000, '3', '61fcead9bd166.png', 'Mulai dari gambar yang semarak hingga gambar yang jernih - Anda akan menikmati detail dan tekstur yang halus, bahkan dalam area gambar yang datar. Clear Resolution Enhancer menskala gambar resolusi rendah hingga resolusi Full HD tanpa memperkenalkan noise gambar tambahan. Melakukannya dengan mengura', 2100),
-(37, 'Panci Annons', 91, 99900, '10', '61fdeb89a87ef.png', 'Panci dengan penutup, kaca/baja tahan karat, 2.8 l', 2100);
+(30, 'Pocophone X3 NFC', 38, 4000000, '2', '61f7543fcaa7b.png', 'ditawarkan oleh prosesor baru. Prosesor Qualcomm® Snapdragon™ 732G menawarkan kapasitas pemrosesan grafis yang kuat dan kemampuan komputasi AI yang canggih. Teknologi proses 8nm dan chip octa-core yang membantu mencapai kecepatan jam 2,3GHz.', 2500),
+(31, 'Mi Band 5', 56, 400000, '9', '61f78aa6876ef.png', 'Baterai 500 mAh, tahan air. Bisa tahan 10 hari pemakaian normal. Tersedia berbagai warna', 1200),
+(32, 'Canon', 52, 3500000, '7', '61f78c64beb65.png', 'Sensor APS-C CMOS 24,1 megapiksel+ perekaman video 4K 45 titik AF semua tipe silang (jendela bidik) dan Dual Pixel CMOS AF (Live View)', 3000),
+(34, 'Lenovo Legion 5', 42, 19000000, '3', '61fa1ee09b2fa.png', 'Engineered to deliver devastation in and out of the arena, the Legion 5 Pro deploys AMD Ryzen™ processing and NVIDIA® GeForce RTX™ graphics to dish out high-resolution gaming. The world’s first 16&amp;quot; QHD gaming laptop with up to 165Hz refresh sets up a “winning zone” that gives you an extra e', 1500),
+(35, 'Pop It', 114, 20000, '6', '61fccbe0b0ec4.png', 'Ini adalah mainan yang viral pada masanya', 200),
+(36, 'Televisi Panasonic', 40, 3500000, '3', '61fcead9bd166.png', 'Mulai dari gambar yang semarak hingga gambar yang jernih - Anda akan menikmati detail dan tekstur yang halus, bahkan dalam area gambar yang datar. Clear Resolution Enhancer menskala gambar resolusi rendah hingga resolusi Full HD tanpa memperkenalkan noise gambar tambahan. Melakukannya dengan mengura', 2100),
+(37, 'Panci Annons', 78, 99900, '10', '61fdeb89a87ef.png', 'Panci dengan penutup, kaca/baja tahan karat, 2.8 l', 2100);
 
 -- --------------------------------------------------------
 
@@ -59,29 +59,19 @@ INSERT INTO `barang` (`id`, `nama`, `stok`, `harga`, `kategori_id`, `image`, `de
 
 CREATE TABLE `buktipembayaran` (
   `id_pembayaran` int(10) NOT NULL,
-  `iduser` int(10) NOT NULL,
   `gambar` varchar(100) NOT NULL,
-  `status` varchar(50) NOT NULL,
-  `produk` varchar(50) NOT NULL,
-  `qty` int(10) NOT NULL,
-  `alamat` varchar(200) NOT NULL
+  `tanggal_bayar` varchar(50) NOT NULL,
+  `id_transaksi` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `buktipembayaran`
 --
 
-INSERT INTO `buktipembayaran` (`id_pembayaran`, `iduser`, `gambar`, `status`, `produk`, `qty`, `alamat`) VALUES
-(128, 6, '62130745a038f.png', 'sudah dibayar', 'Pocophone X3 NFC', 3, 'Banjarmlati LengkongKabupaten Kulon Progo'),
-(129, 6, '62130745a038f.png', 'sudah dibayar', 'Pop It', 4, 'Banjarmlati LengkongKabupaten Kulon Progo'),
-(130, 6, '62130745a038f.png', 'sudah dibayar', 'Lenovo Legion 5', 1, 'Banjarmlati LengkongKabupaten Kulon Progo'),
-(131, 6, '621309b0bc323.png', 'sudah dibayar', 'Pocophone X3 NFC', 3, 'iKota Jambi'),
-(132, 6, '62130a55b64f8.png', 'sudah dibayar', 'Lenovo Legion 5', 3, 'iKabupaten Garut'),
-(133, 6, '62130c462d1f2.png', 'sudah dibayar', 'Pocophone X3 NFC', 2, 'iKabupaten Malinau'),
-(134, 6, '62130c876d78b.png', 'sudah dibayar', 'Mi Band 5', 2, 'iKabupaten Mojokerto'),
-(135, 6, '621310ca1a92d.png', 'sudah dibayar', 'Mi Band 5', 2, ' Jln. SudirmanKota Denpasar'),
-(136, 6, '621310ca1a92d.png', 'sudah dibayar', 'Pop It', 2, ' Jln. SudirmanKota Denpasar'),
-(137, 8, '621311bf7ff1d.png', 'sudah dibayar', 'Pocophone X3 NFC', 3, ' Jln pahlawanKota Banjarbaru');
+INSERT INTO `buktipembayaran` (`id_pembayaran`, `gambar`, `tanggal_bayar`, `id_transaksi`) VALUES
+(170, '6215f07cd640a.png', 'Wed Feb 2022', '2302221528006'),
+(172, '6215f26bcb08f.png', 'Wed Feb 2022', '2302221537206'),
+(173, '6215f344eee55.png', 'Wed Feb 2022', '2302221540556');
 
 -- --------------------------------------------------------
 
@@ -136,7 +126,8 @@ CREATE TABLE `ongkir` (
   `nama_kota` varchar(50) NOT NULL,
   `tarif` int(30) NOT NULL,
   `estimasi` varchar(10) NOT NULL,
-  `uid` int(10) NOT NULL
+  `uid` int(10) NOT NULL,
+  `id_transaksi` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -148,20 +139,30 @@ CREATE TABLE `ongkir` (
 CREATE TABLE `pembelian` (
   `id_pembelian` int(20) NOT NULL,
   `user_id` int(30) NOT NULL,
-  `id_ongkir` int(10) NOT NULL,
-  `tanggal` varchar(40) NOT NULL,
-  `total_pembelian` int(30) NOT NULL
+  `id_transaksi` varchar(20) NOT NULL,
+  `id_barang` int(10) NOT NULL,
+  `tanggal` varchar(50) NOT NULL,
+  `total` int(20) NOT NULL,
+  `ongkir` int(11) NOT NULL,
+  `qty` int(10) NOT NULL,
+  `status` varchar(30) NOT NULL,
+  `alamat` varchar(60) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `pembelian`
 --
 
-INSERT INTO `pembelian` (`id_pembelian`, `user_id`, `id_ongkir`, `tanggal`, `total_pembelian`) VALUES
-(33, 6, 210220226, 'Mon Feb 2022', 8350000),
-(34, 6, 210220226, 'Mon Feb 2022', 830000),
-(35, 6, 210220226, 'Mon Feb 2022', 954000),
-(36, 8, 210220228, 'Mon Feb 2022', 12464000);
+INSERT INTO `pembelian` (`id_pembelian`, `user_id`, `id_transaksi`, `id_barang`, `tanggal`, `total`, `ongkir`, `qty`, `status`, `alamat`) VALUES
+(64, 6, '2302221528006', 30, 'Wed Feb 2022', 5500000, 300000, 1, 'proses pengisisan data', 'Banjarmlati Lengkong'),
+(65, 6, '2302221528006', 31, 'Wed Feb 2022', 5500000, 300000, 3, 'proses pengisisan data', 'Banjarmlati Lengkong'),
+(69, 6, '2302221537206', 30, 'Wed Feb 2022', 18879800, 180000, 2, 'proses pengisisan data', 'jln pahlawan'),
+(70, 6, '2302221537206', 32, 'Wed Feb 2022', 18879800, 180000, 3, 'proses pengisisan data', 'jln pahlawan'),
+(71, 6, '2302221537206', 37, 'Wed Feb 2022', 18879800, 180000, 2, 'proses pengisisan data', 'jln pahlawan'),
+(72, 6, '2302221540556', 36, 'Wed Feb 2022', 16823900, 924000, 2, 'proses pengisisan data', 'jln sumpah pemuda'),
+(73, 6, '2302221540556', 37, 'Wed Feb 2022', 16823900, 924000, 1, 'proses pengisisan data', 'jln sumpah pemuda'),
+(74, 6, '2302221540556', 30, 'Wed Feb 2022', 16823900, 924000, 2, 'proses pengisisan data', 'jln sumpah pemuda'),
+(75, 6, '2302221540556', 31, 'Wed Feb 2022', 16823900, 924000, 2, 'proses pengisisan data', 'jln sumpah pemuda');
 
 -- --------------------------------------------------------
 
@@ -300,13 +301,13 @@ ALTER TABLE `barang`
 -- AUTO_INCREMENT for table `buktipembayaran`
 --
 ALTER TABLE `buktipembayaran`
-  MODIFY `id_pembayaran` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=138;
+  MODIFY `id_pembayaran` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=174;
 
 --
 -- AUTO_INCREMENT for table `cart`
 --
 ALTER TABLE `cart`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=130;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=178;
 
 --
 -- AUTO_INCREMENT for table `kategori`
@@ -318,19 +319,19 @@ ALTER TABLE `kategori`
 -- AUTO_INCREMENT for table `ongkir`
 --
 ALTER TABLE `ongkir`
-  MODIFY `id_ongkir` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=50;
+  MODIFY `id_ongkir` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=80;
 
 --
 -- AUTO_INCREMENT for table `pembelian`
 --
 ALTER TABLE `pembelian`
-  MODIFY `id_pembelian` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=37;
+  MODIFY `id_pembelian` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=76;
 
 --
 -- AUTO_INCREMENT for table `sold`
 --
 ALTER TABLE `sold`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=57;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=107;
 
 --
 -- AUTO_INCREMENT for table `stok`
